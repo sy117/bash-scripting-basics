f3f0b1a07ad0514e5c21bc2c46a869ab0da6d9fa
sy117 <07sunilraj@gmail.com>
2026-03-01 16:34:27 +0530

    Added comments for different flags used

--- practice-problems/intermediate/network_health_checker/soln_script.sh
+++ practice-problems/intermediate/network_health_checker/soln_script.sh
@@ -1,6 +1,9 @@
 # /bin/bash
 
 HOSTS=("google.com" "8.8.8.8" "github.com" "stackoverflow.com" "leetcode.com" "localhost")
+
+# Format string for table output
+# %-Xs → left align string in X width column
 FORMAT="%-20s %-15s %-12s\n"
 
 printf "\n$FORMAT" "HOST" "STATUS" "LATENCY(ms)"
@@ -14,9 +17,13 @@ else
 fi
 
 for host in "${HOSTS[@]}"; do
+    # -c 1 → send only 1 packet
     output=$(ping -c 1 -W $TIMEOUT "$host" 2>/dev/null)
 
     if [ $? -eq 0 ]; then
+        # grep -o → print only matching part
+        # 'time=[0-9.]*' → match time=number
+        # cut -d= -f2 → split by '=' and take 2nd field
         latency=$(echo "$output" | grep -o 'time=[0-9.]*' | cut -d= -f2)
         printf "$FORMAT" "$host" "UP" "$latency"
     else
